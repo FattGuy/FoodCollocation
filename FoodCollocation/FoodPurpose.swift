bf002748a0dbd7f6002b2dbc6d63d3b0579c0f79
Feng Chang <fengchang@fengs-macbook-pro.local>
2016-05-27 12:39:22 -0500

    finished the basic food purposes

---
+++ FoodCollocation/FoodPurpose.swift
@@ -0,0 +1,18 @@
+//
+//  FoodPurpose.swift
+//  FoodCollocation
+//
+//  Created by Feng Chang on 5/27/16.
+//  Copyright © 2016 Feng Chang. All rights reserved.
+//
+
+import UIKit
+
+class FoodPurpose: NSObject {
+
+    var purpose: String?
+    
+    init(purpose: String) {
+        self.purpose = purpose
+    }
+}
--- FoodCollocation/ViewController.swift
+++ FoodCollocation/ViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonClicked(sender: UIButton) {
+        self.performSegueWithIdentifier("toSecondPageSegue", sender: sender)
+    }
 }
 
